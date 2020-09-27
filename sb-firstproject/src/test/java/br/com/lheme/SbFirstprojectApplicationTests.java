0850b53c7c79233a189197d167a08d3f28b001e4
luizleme <luiz.lheme@gmail.com>
2020-09-27 12:13:30 -0300

    Created the first Spring Batch Project

---
+++ sb-firstproject/src/test/java/br/com/lheme/SbFirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lheme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbFirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

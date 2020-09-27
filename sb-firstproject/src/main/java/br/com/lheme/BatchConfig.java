d10a56d0913a817e2c9afbc3c5f573472147bab9
luizleme <luiz.lheme@gmail.com>
2020-09-27 15:49:59 -0300

    Changed the application and added an incremeter to increment the ID at the database to run more times

--- sb-firstproject/src/main/java/br/com/lheme/BatchConfig.java
+++ sb-firstproject/src/main/java/br/com/lheme/BatchConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -25,12 +26,17 @@ public class BatchConfig {
 
 	@Bean
 	public Job printAtConsoleJob() {
-		return jobBuilderFactory.get("printAtConsoleJob").start(printAtConsoleStep()).build();
+		return jobBuilderFactory
+				.get("printAtConsoleJob")
+				.start(printAtConsoleStep())
+				.incrementer(new RunIdIncrementer())
+				.build();
 	}
 
 	public Step printAtConsoleStep() {
-		return stepBuilderFactory.get("printAtConsoleStep").tasklet(new Tasklet() {
-
+		return stepBuilderFactory
+				.get("printAtConsoleStep")
+				.tasklet(new Tasklet() {
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				System.out.println("First Spring Bacth Project");

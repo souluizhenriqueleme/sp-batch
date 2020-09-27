6b4773c8b473dc37969d8886f29b0ec62bb6307c
luizleme <luiz.lheme@gmail.com>
2020-09-27 16:05:21 -0300

    Refactor of the code and extracted the Tasklet code into a new method, and included the code to obtain the scope parameters

--- sb-firstproject/src/main/java/br/com/lheme/BatchConfig.java
+++ sb-firstproject/src/main/java/br/com/lheme/BatchConfig.java
@@ -6,11 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,22 +28,23 @@ public class BatchConfig {
 
 	@Bean
 	public Job printAtConsoleJob() {
-		return jobBuilderFactory
-				.get("printAtConsoleJob")
-				.start(printAtConsoleStep())
-				.incrementer(new RunIdIncrementer())
-				.build();
+		return jobBuilderFactory.get("printAtConsoleJob").start(printAtConsoleStep())
+				.incrementer(new RunIdIncrementer()).build();
 	}
 
 	public Step printAtConsoleStep() {
-		return stepBuilderFactory
-				.get("printAtConsoleStep")
-				.tasklet(new Tasklet() {
+		return stepBuilderFactory.get("printAtConsoleStep").tasklet(printAtConsoleTasklet(null)).build();
+	}
+
+	@StepScope
+	@Bean
+	public Tasklet printAtConsoleTasklet(@Value("#{jobParameters['nome']}") String nome) {
+		return new Tasklet() {
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("First Spring Bacth Project");
+				System.out.println(String.format("First Spring Bacth Project, %s",nome));
 				return RepeatStatus.FINISHED;
 			}
-		}).build();
+		};
 	}
 }

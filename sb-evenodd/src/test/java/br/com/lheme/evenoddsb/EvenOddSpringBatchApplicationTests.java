c02c0d3f11b8b60517737640b1c37e69de8f0483
luizleme <luiz.lheme@gmail.com>
2020-09-28 19:21:14 -0300

    Created the project sb-evenodd to implement the new code about Spring Batch Processing

---
+++ sb-evenodd/src/test/java/br/com/lheme/evenoddsb/EvenOddSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lheme.evenoddsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenOddSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
